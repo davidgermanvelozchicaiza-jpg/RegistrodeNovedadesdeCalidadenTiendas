e19f54d8a0b73d9baebcdee132543fb875e9d84f
davidgermanvelozchicaiza-jpg <davidgermanvelozchicaiza@gmail.com>
2026-03-17 14:11:59 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'deprati-v13';  // ← incrementar esto fuerza actualización en dispositivos instalados
+const CACHE_NAME = 'deprati-v14';  // ← incrementar esto fuerza actualización en dispositivos instalados
 
 self.addEventListener('install', e => {
   e.waitUntil(
